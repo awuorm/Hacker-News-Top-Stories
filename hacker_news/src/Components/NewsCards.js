330caf02200c9a0e64c43feeae640f1f2f454020
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-09-22 16:18:20 +0300

    created Navbar, footersection and newscard components

---
+++ hacker_news/src/Components/NewsCards.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+
+
+function NewsCards() {
+
+
+
+
+  return (
+    <div>
+      NewsCards
+    </div>
+  );
+}
+
+export default NewsCards;
